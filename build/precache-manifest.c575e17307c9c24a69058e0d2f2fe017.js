956f7965cc4ced6dd00ce9e681e855f0436642fe
Halim <46631802+HalimHamidov@users.noreply.github.com>
2020-10-18 20:37:37 +0300

    feat: my first deploy of react to gh-pages

---
+++ build/precache-manifest.c575e17307c9c24a69058e0d2f2fe017.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "65efc01008f0e35f1304da949fd87c7f",
+    "url": "/Joke/index.html"
+  },
+  {
+    "revision": "4865f52ebb92a041e629",
+    "url": "/Joke/static/css/main.7b1eadba.chunk.css"
+  },
+  {
+    "revision": "07d823f68b096e5aebf8",
+    "url": "/Joke/static/js/2.7b21890b.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/Joke/static/js/2.7b21890b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4865f52ebb92a041e629",
+    "url": "/Joke/static/js/main.383b9fac.chunk.js"
+  },
+  {
+    "revision": "9a8d0b12dbcb8e8abc62",
+    "url": "/Joke/static/js/runtime-main.6e268a8a.js"
+  }
+]);
